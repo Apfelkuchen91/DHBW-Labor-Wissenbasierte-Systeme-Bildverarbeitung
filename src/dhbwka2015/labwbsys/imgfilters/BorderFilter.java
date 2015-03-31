dcbb2acf4adac82913ad655fb60bbc497085147b
Apfelkuchen91 <apfelkuchen91@t-online.de>
2015-03-31 12:22:57 +0200

    Add parameter to border filter.

--- src/dhbwka2015/labwbsys/imgfilters/BorderFilter.java
+++ src/dhbwka2015/labwbsys/imgfilters/BorderFilter.java
@@ -1,5 +1,7 @@
 package dhbwka2015.labwbsys.imgfilters;
 
+import com.sun.javafx.scene.layout.region.Margins;
+
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
@@ -15,7 +17,8 @@ public class BorderFilter implements ImageFilterIf {
         ColorModel model = in.getColorModel();
         ColorModel outmodel = out.getColorModel();
 
-        int borderWidth = 10;
+        int borderWidth = Integer.parseInt(parameters.get(0));
+        RGB borderRGB = new RGB(Integer.parseInt(parameters.get(1)), Integer.parseInt(parameters.get(2)), Integer.parseInt(parameters.get(3)), Integer.parseInt(parameters.get(4)));
         int iImageStart = borderWidth;
         int iImageEnde = in.getWidth() - borderWidth;
         int jImageStart = borderWidth;
@@ -23,19 +26,14 @@ public class BorderFilter implements ImageFilterIf {
 
         for (int i = 0; i < iImageStart; ++i) {
             for (int j = 0; j < in.getHeight(); ++j) {
-
-                RGB newRgb = new RGB(255, 255, 0, 0);
-
-                outRaster.setDataElements(i, j, outmodel.getDataElements(newRgb.getRGB(), null));
+                outRaster.setDataElements(i, j, outmodel.getDataElements(borderRGB.getRGB(), null));
             }
         }
 
         for (int i = iImageStart; i < iImageEnde; ++i) {
             for (int j = 0; j < jImageStart; ++j) {
 
-                RGB newRgb = new RGB(255, 255, 0, 0);
-
-                outRaster.setDataElements(i, j, outmodel.getDataElements(newRgb.getRGB(), null));
+                outRaster.setDataElements(i, j, outmodel.getDataElements(borderRGB.getRGB(), null));
             }
 
             for (int j = jImageStart; j < jImageEnde; ++j) {
@@ -49,18 +47,16 @@ public class BorderFilter implements ImageFilterIf {
 
             for (int j = jImageEnde; j < in.getHeight(); ++j) {
 
-                RGB newRgb = new RGB(255, 255, 0, 0);
 
-                outRaster.setDataElements(i, j, outmodel.getDataElements(newRgb.getRGB(), null));
+                outRaster.setDataElements(i, j, outmodel.getDataElements(borderRGB.getRGB(), null));
             }
         }
 
         for (int i = iImageEnde; i < in.getWidth(); ++i) {
             for (int j = 0; j < in.getHeight(); ++j) {
 
-                RGB newRgb = new RGB(255, 255, 0, 0);
 
-                outRaster.setDataElements(i, j, outmodel.getDataElements(newRgb.getRGB(), null));
+                outRaster.setDataElements(i, j, outmodel.getDataElements(borderRGB.getRGB(), null));
             }
         }
     }
